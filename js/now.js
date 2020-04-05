78bc4024fee3e364b3e48933b6d39df5e965b1b8
Vladimir Penkin <penkinv@gmail.com>
2020-04-05 22:07:44 +0300

    Redesign of the site

---
+++ js/now.js
@@ -0,0 +1,60 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Now JS
+/*
+-----------------------------------------------------------------------------------*/
+
+ jQuery(document).ready(function($) {
+
+/*----------------------------------------------------*/
+/* FitText Settings
+------------------------------------------------------ */
+
+    setTimeout(function() {
+        $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+    }, 100);
+
+
+/*----------------------------------------------------*/
+/*	Make sure that #header-background-image height is
+/* equal to the browser height.
+------------------------------------------------------ */
+
+   $(window).on('resize', function() {
+        $('body').css({ 'width': $(window).width() })
+   });
+
+
+/*----------------------------------------------------*/
+/*	Fade In/Out Primary Navigation
+------------------------------------------------------*/
+
+   $(window).on('scroll', function() {
+
+		var h = $('header').height();
+		var y = $(window).scrollTop();
+        var nav = $('#nav-wrap');
+
+        if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+            nav.fadeOut('fast');
+        }
+        else {
+            if (y < h*.20) {
+                nav.removeClass('opaque').fadeIn('fast');
+            }
+            else {
+                nav.addClass('opaque').fadeIn('fast');
+            }
+        }
+
+	});
+
+
+});
+
+
+
+
+
+
+
